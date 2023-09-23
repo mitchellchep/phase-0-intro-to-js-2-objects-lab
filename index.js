97151c0f86ea447ceda50995dd4c57330e61536a
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-23 22:30:39 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee =  {
+    name: "Jane Doe",
+    streetAddress: "Ngong road"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee = { ...employee};
+    newEmployee[key] =  value;
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] =  value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    let newEmployee = { ...employee};
+    delete(newEmployee[key]);
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    delete(employee[key]);
+    return employee
+}
+
